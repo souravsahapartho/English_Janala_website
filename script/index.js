fd55a4935dcc80daf6ef0b3e6f2713c80a340240
souravsahapartho <souravsahapartho@gmail.com>
2025-09-21 10:48:25 +0600

    updated

--- script/index.js
+++ script/index.js
@@ -1,3 +1,8 @@
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join(" ");
+};
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all") //promise of response
     .then((res) => res.json()) //promise of json data
@@ -34,7 +39,11 @@ const displayWordDetails = (word) => {
   const detailsBox = document.getElementById("details-container");
   detailsBox.innerHTML = `
     <div class="">
-            <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>: ${word.pronunciation})</h2>
+            <h2 class="text-2xl font-bold">${
+              word.word
+            } (<i class="fa-solid fa-microphone-lines"></i>: ${
+    word.pronunciation
+  })</h2>
           </div>
           <div class="">
             <h2 class="font-bold">Meaning</h2>
@@ -46,9 +55,9 @@ const displayWordDetails = (word) => {
           </div>
           <div class="">
             <h2 class="font-bold">Synonym</h2>
-            <span class="btn">Syn1</span>
-            <span class="btn">Syn1</span>
-            <span class="btn">Syn1</span>
+                <div class="">
+            ${createElements(word.synonyms)}
+          </div>
           </div>
   
   `;
